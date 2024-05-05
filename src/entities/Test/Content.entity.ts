d12c2dbffd5a2cb216a96ffcb6f29b13ecfa9602
maabChaoui <maab.chaoui@ensia.edu.dz>
2024-05-05 15:50:36 +0100

    experimented with @ChildEntity, concluded against it

---
+++ src/entities/Test/Content.entity.ts
@@ -0,0 +1,38 @@
+import {
+  ChildEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  TableInheritance,
+} from "typeorm";
+
+@Entity()
+@TableInheritance({ column: { type: "varchar", name: "type" } })
+export class Content {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+}
+
+@ChildEntity()
+export class Photo extends Content {
+  @Column()
+  size: string;
+}
+
+@ChildEntity()
+export class Question extends Content {
+  @Column()
+  answersCount: number;
+}
+
+@ChildEntity()
+export class Post extends Content {
+  @Column()
+  viewCount: number;
+}
--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -4,6 +4,8 @@ import { createTag, getTagsFromList } from "../services/tag.services";
 import { findUserByUid } from "../services/user.services";
 import { Role } from "../entities/User/Roles.entity";
 import { assignRoleToUser, createRole } from "../services/role.services";
+import { AppDataSource } from "../utils/data-source";
+import { Content, Photo } from "../entities/Test/Content.entity";
 
 const router = Router();
 
@@ -33,12 +35,6 @@ router.post("/createTag", async (req: Request, res: Response) => {
     });
   }
 });
-
-// router.all("/problemId", (req: Request, res: Response) => {
-//   const problem = new Problem();
-
-//   res.status(200).json({ so: problem.id ?? "shame" });
-// });
 
 router.post("/createRole", async (req: Request, res: Response) => {
   const { roleName } = req.body;
@@ -73,6 +69,57 @@ router.post("/assignRoleToUser", async (req: Request, res: Response) => {
       message: error.message,
     });
   }
+});
+
+const contentRepo = AppDataSource.getRepository(Content);
+const photoRepo = AppDataSource.getRepository(Photo);
+
+router.get("/createContent", async (req: Request, res: Response) => {
+  const content = new Content();
+  content.description = "DESC";
+  content.title = "Here is a title";
+
+  await contentRepo.save(content);
+
+  res.json({ body: content });
+});
+
+router.get("/createPhoto", async (req: Request, res: Response) => {
+  const photo = new Photo();
+  photo.description = "cover photo";
+  photo.title = "OMG it is working";
+  photo.size = "1920*1080px";
+
+  await photoRepo.save(photo);
+
+  res.json({ body: photo });
+});
+
+router.get("/photos", async (req: Request, res: Response) => {
+  const photos = await photoRepo.find();
+
+  res.json({ body: photos });
+});
+
+router.get("/content", async (req: Request, res: Response) => {
+  const contents = await contentRepo.find();
+
+  res.json({ body: contents });
+});
+
+router.get("/photoFromContent", async (req: Request, res: Response) => {
+  const c = await contentRepo.findOneBy({ id: 1 });
+
+  const photoLike = { ...c, size: "1024x768" }; // Attempt to add Photo properties
+  const photo = photoRepo.merge(new Photo(), photoLike);
+  const contents = await contentRepo.find();
+
+  await photoRepo.save(photo);
+  await contentRepo.delete({
+    id: c?.id ?? 0,
+  });
+
+  res.json({ body: photo });
 });
 
 export default router;
